70207189fd4e913d32c7e997e32999598520bbbf
Dmitry Bushev <bushevdv@gmail.com>
2014-12-22 21:28:12 +0300

    initial commit

---
+++ utopia.hs
@@ -0,0 +1,21 @@
+module Utopia where
+
+readCases :: (Integral a, Read a) => IO a
+readCases = readLn
+
+growCycle :: Integral a => a -> a -> a
+growCycle c h
+  | odd  c = h * 2
+  | even c = h + 1
+
+grow :: Integral a => a -> a -> a
+grow 0 h = h
+grow c h = grown c 1 h where
+  grown 0 _ h = h
+  grown c n h = grown (c - 1) (n + 1) (growCycle n h)
+
+main :: IO ()
+main = do
+  n <- readCases
+  let r = n + 1
+  print r
