1b83c7ec8e79b85ddf2655336cf42c4933eb1e5c
Dmitry Bushev <bushevdv@gmail.com>
2014-12-27 14:10:24 +0300

    add fast AcmIcpcTeam based on [Int]

--- AcmIcpcTeam.hs
+++ AcmIcpcTeam.hs
@@ -2,37 +2,62 @@ import Data.Bits
 import Control.Monad
 import Utils
 
-data IInteger = IInteger {ind :: Int, int :: Integer}
-              deriving (Eq, Show)
-
-instance Ord IInteger where
-  compare x y = compare (ind x) (ind y)
-
-fromInteger' :: Integer -> IInteger
-fromInteger' i = IInteger (0) i
-
-fromIntegerIor :: Integer -> IInteger
-fromIntegerIor i = IInteger (popCount i) i
-
-ior :: IInteger -> IInteger -> IInteger
-ior x y = let o = (int x) .|. (int y)
-          in fromIntegerIor o
-
-readBinaryString :: IO IInteger
-readBinaryString = fmap binToIInt getLine
-
-binToIInt :: String -> IInteger
-binToIInt [] = IInteger 0 0
-binToIInt bs = let ind  = zip [0..] $ reverse bs
-                   pop = map fst $ filter (\p -> snd p == '1') ind
-               in fromInteger' $ foldr (flip setBit) 0 pop
+fixedWindow :: Int -> [a] -> [[a]]
+fixedWindow _ [] = []
+fixedWindow n xs = let (l,r) = splitAt n xs
+                   in l : fixedWindow n r
 
 combinations :: (a -> a -> a) -> [a] -> [a]
 combinations _ []     = []
 combinations f (x:xs) = map (f x) xs ++ combinations f xs
 
-bitCombinations :: [IInteger] -> [IInteger]
-bitCombinations = combinations ior
+type Bit = (Int, Bool)
+
+mkBit :: Int -> Char -> Bit
+mkBit i '0' = (i, False)
+mkBit i '1' = (i, True)
+mkBit _ c   = error $ show c ++ " is not a valid binary Char"
+
+mkBits :: String -> [Bit]
+mkBits = map (uncurry mkBit) . zip [0..]
+
+significantBits :: [Bit] -> [Int]
+significantBits = map fst . filter snd
+
+data IInt = IInt {bits :: [Int]} deriving (Eq, Show)
+
+instance Ord IInt where
+  compare x y = compare (pop x) (pop y)
+
+pop :: IInt -> Int
+pop = sum . map popCount . bits
+
+-- from bit sequence
+mkIInt2 :: [[Bit]] -> IInt
+mkIInt2 = mkIInt1 . map significantBits
+
+-- from indices
+mkIInt1 :: [[Int]] -> IInt
+mkIInt1 = mkIInt . map (foldl setBit 0)
+
+-- from bits
+mkIInt :: [Int] -> IInt
+mkIInt xs = IInt xs
+
+bitOr :: IInt -> IInt -> IInt
+bitOr x y = mkIInt $ zipWith (.|.) (bits x) (bits y)
+
+binToIInt :: String -> IInt
+binToIInt xs = let intSize = finiteBitSize (0::Int)
+                   chunkss = fixedWindow intSize $ reverse xs
+                   bitss   = map mkBits chunkss
+               in mkIInt2 bitss
+
+bitCombinations :: [IInt] -> [IInt]
+bitCombinations = combinations bitOr
+
+readBinaryString :: IO IInt
+readBinaryString = fmap binToIInt getLine
 
 main :: IO ()
 main = do
@@ -41,5 +66,5 @@ main = do
   let coms = bitCombinations xs
   let best = maximum coms
   let top = filter (==best) coms
-  print $ ind best
+  print $ pop best
   print $ length top
