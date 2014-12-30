0aadaa89962564dff04f42c6eb1e2c0e44fe640a
Dmitry Bushev <bushevdv@gmail.com>
2014-12-30 22:14:07 +0300

    add haskell profiling options

--- src/main/haskell/algorithms/dynamic-programming/MaximumSubarray.hs
+++ src/main/haskell/algorithms/dynamic-programming/MaximumSubarray.hs
@@ -11,19 +11,24 @@ readInput :: Int -> IO [[Int]]
 readInput n = replicateM n $ getLine >> readSeqInt
 
 maxSubseq :: [Int] -> Int
-maxSubseq xs = maxSubseq' xs 0 0 0 (minBound :: Int) (0, 0)
+maxSubseq xs = fst $ maxSubseq' xs 0 0 0 (minBound :: Int) 0 0
 
-maxSubseq' :: [Int] -> Int -> Int -> Int -> Int -> (Int, Int) -> Int
-maxSubseq' [] _ _ _ maxSum _ = maxSum
-maxSubseq' (x:xs) curSrt curEnd curSum maxSum (maxSrt,maxEnd) =
+maxSubseq' :: [Int] ->
+              Int -> Int -> Int ->
+              Int -> Int -> Int ->
+              (Int, (Int, Int))
+maxSubseq' [] _ _ _ maxSum maxSrt maxEnd = (maxSum, (maxSrt, maxEnd))
+maxSubseq' (x:xs) curSrt curEnd curSum maxSum maxSrt maxEnd =
   let curSum' = curSum + x
       curEnd' = curEnd + 1
-      (maxSum',maxSrt',maxEnd') = if curSum' > maxSum then (curSum',curSrt,curEnd) else (maxSum,maxSrt,maxEnd)
-      (curSumn', curSrt') = if curSum' < 0 then (0, curEnd+1) else (curSum',curSrt)
-  in maxSubseq' xs curSrt' curEnd' curSumn' maxSum' (maxSrt', maxEnd')
+      (maxSum', maxSrt', maxEnd') = if curSum' > maxSum
+                                    then (curSum', curSrt, curEnd)
+                                    else (maxSum, maxSrt, maxEnd)
+      (curSum1', curSrt') = if curSum' < 0 then (0, curEnd+1) else (curSum', curSrt)
+  in maxSubseq' xs curSrt' curEnd' curSum1' maxSum' maxSrt' maxEnd'
 
-maxNonSubseq :: [Int] -> Int
-maxNonSubseq xs
+maxNoncontigSubseq :: [Int] -> Int
+maxNoncontigSubseq xs
   | all (<0) xs = maximum xs
   | otherwise   = foldr' sumPositive 0 xs
   where sumPositive x s = if x > 0 then x+s else s
@@ -33,8 +38,8 @@ showTuple (x,y) = show x ++ " " ++ show y
 
 main :: IO ()
 main = do
-  t <- readLn
+  t  <- readLn
   xs <- readInput t
   let ss = map maxSubseq xs
-      ns = map maxNonSubseq xs
+      ns = map maxNoncontigSubseq xs
   forM_ (zip ss ns) $ putStrLn . showTuple
