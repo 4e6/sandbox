e3e75eeb817671d6f55afb673589301df572dbb4
Dmitry Bushev <bushevdv@gmail.com>
2015-01-06 00:35:41 +0300

    complete EvenTree

--- hackerrank/src/main/haskell/algorithms/graph-theory/EvenTree.hs
+++ hackerrank/src/main/haskell/algorithms/graph-theory/EvenTree.hs
@@ -7,65 +7,56 @@ import qualified Sandbox.Util.IO as U
 type Vertex = Int
 type Edge   = (Vertex,Vertex)
 
-data Tree = Node {vertex :: Vertex, edges :: [Tree]} deriving (Show)
+data Tree = Node {vertex :: Vertex, edges :: [Tree]} deriving (Eq, Show)
 
 single :: Vertex -> Tree
 single x = Node x []
 
---splice :: Tree -> Tree -> Tree
---splice from (Node v es) = Node v (from:es)
+isEven :: Tree -> Bool
+isEven = even . size
 
 size :: Tree -> Int
 size t = 1 + sum (map size (edges t))
-
-mkTree :: [Edge] -> Tree
-mkTree es = insertAll es $ single 1
-
-insertAll :: [Edge] -> Tree -> Tree
-insertAll xs t = foldl (flip insert) t xs
-
-insert :: Edge -> Tree -> Tree
-insert e@(from,to) tree@(Node v es)
-  | to == v   = Node v $ single from : es
-  | otherwise = Node v $ map (insert e) es
-
-remove :: Tree -> Tree -> Tree
-remove xt@(Node x _) (Node v es) = let p = (==x) . vertex
-                                   in if any p es
-                                      then Node v $ filter (not . p) es
-                                      else Node v $ map (remove xt) es
 
 find :: Vertex -> Tree -> Maybe Tree
 find x t@(Node v ss)
   | x == v    = Just t
   | otherwise = Mb.listToMaybe $ Mb.mapMaybe (find x) ss
 
-findParent :: Vertex -> [Edge] -> Maybe Vertex
-findParent _ []         = Nothing
-findParent v ((f,t):es)
-  | v == f    = Just t
-  | otherwise = findParent v es
+insert :: Edge -> Tree -> Tree
+insert e@(from,to) (Node v es)
+  | to == v   = Node v $ single from : es
+  | otherwise = Node v $ map (insert e) es
 
-isEven :: Tree -> Bool
-isEven = even . size
+insertAll :: [Edge] -> Tree -> Tree
+insertAll xs t = foldl (flip insert) t xs
 
-findEven :: Vertex -> [Edge] -> Tree-> Maybe Tree
-findEven v es t = case find v t of
-                   Nothing -> Nothing
-                   Just t' -> if isEven t'
-                              then Just t'
-                              else case findParent v es of
-                                    Nothing -> Nothing
-                                    Just p  -> findEven p es t
+mkTree :: [Edge] -> Tree
+mkTree es = insertAll es $ single 1
 
-evenForest :: [Edge] -> Tree -> [Tree] -> [Tree]
-evenForest [] t as = as
-evenForest es'@((fr,to):es) t as = case findEven fr es' t of
-                                    Nothing -> evenForest es t as
-                                    Just t' -> evenForest es (remove t' t) (t':as)
+removeVertex :: Vertex -> Tree -> Tree
+removeVertex x (Node v es) = let p = (==x) . vertex
+                             in if any p es
+                                then Node v $ filter (not . p) es
+                                else Node v $ map (removeVertex x) es
 
-splitEven :: [Edge] -> Tree -> [Tree]
-splitEven es t = evenForest es t []
+remove :: Tree -> Tree -> Tree
+remove = removeVertex . vertex
+
+splitTree :: Vertex -> Tree -> (Tree, Tree)
+splitTree x t = let Just t' = find x t
+                in (t', removeVertex x t)
+
+splitEven' :: Tree -> [Tree] -> Int -> ([Tree], Int)
+splitEven' t [] n = ([t], n)
+splitEven' t (e:es) n = let (l,r) = splitTree (vertex e) t
+                            merge (ts1,n1) (ts2,n2) = (ts1 ++ ts2, n1 + n2)
+                        in if isEven l && isEven r
+                           then splitEven' l (edges l) n `merge` splitEven' r (edges r) 1
+                           else splitEven' t (es ++ edges e) n
+
+splitEven ::  Tree -> ([Tree], Int)
+splitEven t@(Node _ es) = splitEven' t es 0
 
 -- n - number of vertices
 -- m - number of edges
@@ -73,9 +64,5 @@ main :: IO ()
 main = do
   (n,m) <- U.readPair
   es    <- M.replicateM m U.readPair
-  let forest = splitEven (reverse es) (mkTree es)
-  putStrLn "tree:"
-  print $ mkTree es
-  putStrLn "forest:"
-  M.forM_ forest print
-  print $ length forest
+  let (_, x) = splitEven $ mkTree es
+  print x
