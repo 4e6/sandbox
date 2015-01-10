76644ba060fb003fff029c90ce9c2426965cf670
Dmitry Bushev <bushevdv@gmail.com>
2015-01-10 16:02:28 +0300

    add memoized Divisibility

--- hackerrank/src/main/haskell/algorithms/data-structures/Divisibility.hs
+++ hackerrank/src/main/haskell/algorithms/data-structures/Divisibility.hs
@@ -1,12 +1,31 @@
 module Main (main) where
 
+import Control.Monad.ST
 import qualified Control.Monad as M
+import qualified Data.Map.Strict as Map
+import Data.STRef
+import System.IO.Unsafe
 import Sandbox.Util.IO (readPair)
+
+memoize :: Ord a => (a -> ST s b) -> ST s (a -> ST s b)
+memoize f = do
+  mc <- newSTRef Map.empty
+  return $ \k -> do
+    c <- readSTRef mc
+    case Map.lookup k c of
+     Just  v -> return v
+     Nothing -> do v <- f k
+                   writeSTRef mc (Map.insert k v c) >> return v
+
+unsafeMemoize :: Ord a => (a -> b) -> a -> b
+unsafeMemoize f = unsafePerformIO $ do
+  f' <- stToIO $ memoize $ return . f
+  return $ unsafePerformIO . stToIO . f'
 
 slice :: Integral a => Int -> a -> a -> a -> a
 slice s x b e = let b' = fromIntegral s - b + 1
                     e' = fromIntegral s - e
-                in truncate $ fromIntegral (mod x (10^b')) / fromIntegral (10^e')
+                in quot (mod x (10^b')) (10^e')
 
 intervals :: Integral a => a -> a -> [(a,a)]
 intervals b' e' = intervals_ b' e' b' []
@@ -19,6 +38,9 @@ ints :: Integral a => Int -> a -> a -> a -> [a]
 ints s x b e = let xlice = uncurry $ slice s x
                in map xlice $ intervals b e
 
+intsf :: Integral a => ((a,a) -> a) -> a -> a -> [a]
+intsf f b e = map f $ intervals b e
+
 divisible :: Integral a => a -> a -> Bool
 divisible s p = mod s p == 0
 
@@ -29,6 +51,11 @@ main = do
   pairs <- M.replicateM (fromIntegral q) readPair
   let size' = length str
       s'    = read str :: Integer
-      ints' = uncurry $ ints size' s'
-      divs' = map (length . filter (`divisible` p) . ints') pairs
+      memslice = unsafeMemoize $ uncurry $ slice size' s'
+      divp = (`divisible` p)
+      intsf' = uncurry $ intsf memslice
+--      pints' = map intsf' pairs
+--      ss = map length pints'
+--      divs1 = map (length . filter (`divisible` p)) pints'
+      divs' = map (length . filter divp . intsf') pairs
   M.forM_ divs' print
